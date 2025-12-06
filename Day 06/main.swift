76b993f1a338414a2c39039df3d98865cb3f3f00
Wladyslaw Kuczynski <138483245+vladkleshchanka@users.noreply.github.com>
2025-12-06 11:35:53 +0100

    Day 6: part 2 wip

--- Day 06/main.swift
+++ Day 06/main.swift
@@ -37,4 +37,32 @@ func part1() async throws {
     print(values)
 }
 
+func part2() async throws {
+    var matrix = [[Character]]()
+    var operands = [Character]()
+    var sums = [Int]()
+
+    for try await line in url.lines {
+        if line.contains("+") || line.contains("*") {
+            operands = line.split(separator: " ", omittingEmptySubsequences: true)
+        } else {
+            matrix.append(Array(line))
+        }
+    }
+    let in = """
+    123 328  51 64 
+     45 64  387 23 
+      6 98  215 314
+    *   +   *   +  
+    """
+    var currentBucket = [Int]()
+    let height = matrix.count
+    let width = matrix[0].count
+    for i in (0..<width).inverted() {
+        let value = String((0..<height).map { matrix[i][j] })
+        if let 
+    }
+}
+
 try await part1()
+try await part2()
